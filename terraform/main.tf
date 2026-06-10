dd9deef97c6877c8d82a4fc3e02712057c1a4c49
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2026-06-09 17:14:53 -0700

    Changed to t3.medium, added security group creation, EIP allocation, and explicitly added the iam_instance_profile to the default LabInstanceProfile, which is standard across AWS student accounts, might change it to be less hard-coded later

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,9 +13,35 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_security_group" "minecraft_security_group" {
+  name = "minecraft-sg"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "minecraft_elasticIP" {
+  instance = aws_instance.minecraft_server.id
+  domain   = "vpc"
+}
+
 resource "aws_instance" "minecraft_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.medium"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.medium"
+  iam_instance_profile   = "LabInstanceProfile"
+  vpc_security_group_ids = [aws_security_group.minecraft_security_group.id]
+
 
   tags = {
     Name = "automated-minecraft-server"
